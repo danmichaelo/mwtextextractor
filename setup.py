4dc985aa97953709836294e33fa6dea84d872c32
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-04-04 23:58:41 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='mwtextextractor',
-      version='0.1.1',
+      version='0.1.2',
       description='Extracts body text from MediaWiki wikitext by stripping off templates, html tags, tables, headers, etc.',
       long_description=(
           open('README.rst').read()
